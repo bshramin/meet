cad4816164a797f0c6758f6a6db5af70dac1c6cf
Amin Bashiri <amin.bashiri28@gmail.com>
2025-01-11 21:44:53 -0500

    Add the product overview page

---
+++ MyNotes.md
@@ -0,0 +1,9 @@
+https://nextjs.org/docs/app/building-your-application
+
+https://github.com/KaranChandekar/TicketE-Checkout-Page
+
+https://heroicons.com/outline
+
+https://headlessui.com/v1
+
+https://tailwindui.com/components/ecommerce/components/product-overviews
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
+import ProductOverview from "./components/ProductOverview";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <ProductOverview />
     </>
   );
 }
