1dfa740b6c197b035b89d8677adb897ff562dc5e
Amin Bashiri <amin.bashiri28@gmail.com>
2025-01-12 19:25:08 -0500

    Simplify the frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,8 @@
-import { Hero } from "./components/Hero";
-import { Navbar } from "./components/Navbar";
 import ProductOverview from "./components/ProductOverview";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Hero />
       <ProductOverview />
     </>
   );
