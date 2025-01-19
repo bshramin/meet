f135bc3574dc2f10219b36c82ace64da523f18a8
Amin Bashiri <amin.bashiri28@gmail.com>
2025-01-18 19:22:39 -0500

    Allow cors on backend and make response json

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  // res.render("index", { title: "Express" });
+  res.json({ title: "Express Backend" });
 });
 
 module.exports = router;
