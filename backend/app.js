709aee1687a6cca6ff837786bbe2d0f3628e5f52
Amin Bashiri <amin.bashiri28@gmail.com>
2025-02-02 14:01:45 -0500

    Take cors to the top of middleware list

--- backend/app.js
+++ backend/app.js
@@ -16,12 +16,12 @@ require("dotenv").config();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/product", productRouter);
