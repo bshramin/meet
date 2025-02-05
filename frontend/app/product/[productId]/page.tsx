0a98fc83822b2208379d1cb1cf1dc35ca7f35423
Amin Bashiri <amin.bashiri28@gmail.com>
2025-02-04 22:03:00 -0500

    Add product quantity dropdown

--- frontend/app/product/[productId]/page.tsx
+++ frontend/app/product/[productId]/page.tsx
@@ -18,7 +18,7 @@ const BACKEND_BASE_URL =
 export default function ProductOverview() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [productQuantity, setProductQuantity] = useState(0); // TODO: change
+  const [productQuantity, setProductQuantity] = useState(1);
   const [product, setProduct] = useState({
     id: "",
     merchant_id: "",
@@ -151,34 +151,24 @@ export default function ProductOverview() {
                   Number of sessions
                 </label>
                 <div className="mt-2">
-                  <div className="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-                    <div className="shrink-0 select-none text-base text-gray-500 sm:text-sm/6">
-                      $
-                    </div>
-                    <input
-                      id="price"
-                      name="price"
-                      type="text"
-                      placeholder="0.00"
-                      className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-                    />
-                    <div className="grid shrink-0 grid-cols-1 focus-within:relative">
-                      <select
-                        id="currency"
-                        name="currency"
-                        aria-label="Currency"
-                        className="col-start-1 row-start-1 w-full appearance-none rounded-md py-1.5 pl-3 pr-7 text-base text-gray-500 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                      >
-                        <option>USD</option>
-                        <option>CAD</option>
-                        <option>EUR</option>
-                      </select>
-                      <ChevronDownIcon
-                        aria-hidden="true"
-                        className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                      />
-                    </div>
-                  </div>
+                  <select
+                    value={productQuantity}
+                    onChange={(event) =>
+                      setProductQuantity(parseInt(event.target.value))
+                    }
+                    className="block w-32 rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:ring-blue-500"
+                  >
+                    {Array.from({ length: 20 }, (_, i) => i + 1).map(
+                      (number) => (
+                        <option key={number} value={number}>
+                          {number}
+                        </option>
+                      )
+                    )}
+                  </select>
+                  <p className="mt-2 text-gray-600">
+                    Total Price: {productQuantity * product.price}
+                  </p>
                 </div>
               </div>
 
